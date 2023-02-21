6083bdaa0cc186ddda919fe228b0eaa0cd3ea74b
MMarioLP <am4063228@gmail.com>
2023-02-20 21:28:14 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Customers1 from './componentes/cutomers1.jsx'
+
+
+function App() {
+  return (
+    <div className="App">
+    <Customers1/>
+  
+    </div>
+  );
+}
+
+export default App;
